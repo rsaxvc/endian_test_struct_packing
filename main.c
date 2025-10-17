89b61781f686337290f9ac1e34414bb85f136f8d
Richard Allen <rsaxvc@gmail.com>
2025-10-17 11:36:17 -0500

    moar

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <assert.h>
+#include <endian.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 
+#define CRSF_MAX_CHANNELS   24U      // Maximum number of channels from crsf datastream
+#define CRSF_FRAMELEN_MAX   64U      // maximum possible framelength
+#define CRSF_HEADER_LEN     2U       // header length
+#define CRSF_FRAME_PAYLOAD_MAX (CRSF_FRAMELEN_MAX - CRSF_HEADER_LEN)     // maximum size of t>
+#define CRSF_FRAME_LENGTH_MIN 2 // min value for _frame.length
+#define CRSF_BAUDRATE      416666U
+#define ELRS_BAUDRATE      420000U
+#define CRSF_TX_TIMEOUT    500000U   // the period after which the transmitter is considered >
+#define CRSF_RX_TIMEOUT    150000U   // the period after which the receiver is considered dis>
+
 #define PACKED __attribute__((packed, aligned(1)))
+
 struct Channels12Bit_4Chan {
 #if __BYTE_ORDER == __LITTLE_ENDIAN
   uint32_t ch0 : 12;
@@ -11,27 +23,68 @@ struct Channels12Bit_4Chan {
   uint32_t ch2 : 12;
   uint32_t ch3 : 12;
 #elif __BYTE_ORDER == __BIG_ENDIAN
-  uint32_t ch3 : 12;
-  uint32_t ch2 : 12;
-  uint32_t ch1 : 12;
   uint32_t ch0 : 12;
+  uint32_t ch1 : 12;
+  uint32_t ch2 : 12;
+  uint32_t ch3 : 12;
 #else
 #error "unsupported-endian architecture"
 #endif
 } PACKED;
 
+
+    struct SubsetChannelsFrame {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+        uint8_t starting_channel:5;     // which channel number is the first one in the frame
+        uint8_t res_configuration:2;    // configuration for the RC data resolution (10 - 13 >
+        uint8_t digital_switch_flag:1;  // configuration bit for digital channel
+#elif __BYTE_ORDER == __BIG_ENDIAN
+        uint8_t digital_switch_flag:1;  // configuration bit for digital channel
+        uint8_t res_configuration:2;    // configuration for the RC data resolution (10 - 13 >
+        uint8_t starting_channel:5;     // which channel number is the first one in the frame
+#else
+#error "unsupported-endian architecture"
+#endif
+        uint8_t channels[CRSF_FRAME_PAYLOAD_MAX - 2]; // payload less byte above
+        // uint16_t channel[]:res;      // variable amount of channels (with variable resolut>
+                                        // on the res_configuration) based on the frame size
+        // uint16_t digital_switch_channel[]:10; // digital switch channel
+    } PACKED;
+
+
 int main()
 {
-
-  struct Channels12Bit_4Chan f;
-  for(int i = -1; i < 49; ++i){
+  printf("Testing SubsetChannelsFrame:\n");
+  for(int i = -1; i < 17; ++i){
+    struct SubsetChannelsFrame f;
     memset(&f, 0x00, sizeof(f));
     uint8_t * p = (uint8_t*)&f;
     if(i >= 0 && i < 8 * (int)sizeof(f)) {
       p[i / 8] |= 1 << (i % 8);
     }
-    printf("Bit=%2i ch0=%4u ch1=%4u ch2=%4u ch3=%4u\n", i, f.ch0, f.ch1, f.ch2, f.ch3);
+    printf("Bit=%2i word=0x%02x%02x starting_channel=%4u res_configuration=%4u digital_switch_flag=%u\n",
+        i,
+        p[0],p[1],
+        f.starting_channel,
+        f.res_configuration,
+        f.digital_switch_flag);
   }
+  printf("\n");
+
+  printf("Testing Channels12Bit_4Chan:\n");
+  for(int i = -1; i <= 48; ++i){
+    struct Channels12Bit_4Chan f;
+    memset(&f, 0x00, sizeof(f));
+    uint8_t * p = (uint8_t*)&f;
+    if(i >= 0 && i < 8 * (int)sizeof(f)) {
+      p[i / 8] |= 1 << (i % 8);
+    }
+    printf("Bit=%2i word=0x%02x%02x%02x%02x%02x%02x ch0=0x%04x ch1=0x%04x ch2=0x%04x ch3=0x%04x\n",
+        i,
+        p[0],p[1],p[2],p[3],p[4],p[5],
+        f.ch0, f.ch1, f.ch2, f.ch3);
+  }
+  printf("\n");
 
   return 0;
 }
